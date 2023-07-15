7ddb1308d92c140d8751f5136c7dce1a38466198
Harsh khanagwal <harshkhanagwall29@gmail.com>
2023-07-15 16:57:32 +0530

    header design

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import './header.css'
+import Saerchbar from './HeaderSearchBar/Saerchbar'
+import Headerbt from './headerBts/Headerbt'
 
 export const Header = () => {
   return (
     <header className='main mainBackground'>
-        <div className="innerHeader container">
-            hello   
+        <div className="container innerHeader">
+            <div className="logo-area">
+              <img src="/images/logo.png" alt="logo" height={50} />
+            </div>
+            <div className="searchBar">
+              <Saerchbar/>
+            </div>
+            <div className="headerIcons">
+              <Headerbt/>
+            </div>
         </div>
     </header>
   )
