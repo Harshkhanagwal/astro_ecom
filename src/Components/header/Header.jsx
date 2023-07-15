8eddc7ddca25084dc7166ec8781b15f3924a8a3e
Harsh khanagwal <harshkhanagwall29@gmail.com>
2023-07-15 17:23:47 +0530

    search bar ui update

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -2,22 +2,36 @@ import React from 'react'
 import './header.css'
 import Saerchbar from './HeaderSearchBar/Saerchbar'
 import Headerbt from './headerBts/Headerbt'
+import { AiOutlineSearch } from 'react-icons/ai';
 
 export const Header = () => {
   return (
-    <header className='main mainBackground'>
+    <>
+      <header className='main mainBackground'>
         <div className="container innerHeader">
-            <div className="logo-area">
-              <img src="/images/logo.png" alt="logo" height={50} />
-            </div>
-            <div className="searchBar">
-              <Saerchbar/>
-            </div>
-            <div className="headerIcons">
-              <Headerbt/>
-            </div>
+          <div className="logo-area">
+            <img src="/images/logo.png" alt="logo" height={50} />
+          </div>
+          <div className="searchBar">
+            <Saerchbar />
+          </div>
+          <div className="headerIcons">
+            <Headerbt />
+          </div>
         </div>
-    </header>
+      </header>
+      <div className="mobilesearchbar">
+        <div className="serachbar-mobile">
+          <form className='container'>
+            <input type="text" placeholder='Search Your Product' />
+            <button type='submit' className='mobile-searchbt'>
+              <AiOutlineSearch color='white' />
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+
   )
 }
 
