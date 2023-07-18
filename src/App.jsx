35c13e802a3ab3372778e6cf8e2403119d83e649
Sourav-Goyal19 <goyalsourav935@gmail.com>
2023-07-18 14:34:36 +0530

    Navbar Creation

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,18 @@ import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 //components
 import Home from './pages/Home'
 import Productpage from './pages/Productpage'
-import {Header} from './Components/header/Header'
+import { Header } from './Components/header/Header'
+import Navbar from './Components/nav/Navbar'
 
 //stylesheets
 import './style.css'
-
 
 const App = () => {
     return (
         <>
             <Router>
                 <Header />
+                <Navbar />
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='/product' element={<Productpage />} />
---
+++ src/Components/nav/Navbar.jsx
@@ -0,0 +1,18 @@
+import './Navbar.css'
+const Navbar = () => {
+    return (
+        <div className='Navbar'>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>All Products</li>
+                    <li>Men's Collection</li>
+                    <li>Women's Collection</li>
+                    <li>About</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
